354a431c8c5c8173fbe4b06d175a33a14ea9089a
Satwik Samanta <85541670+akasatwik@users.noreply.github.com>
2025-01-19 20:27:50 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Testnet Faucet</title>
-        <meta name="description" content="monad devnet Facuet" />
+        <title>STORY PROTOCOL Testnet Faucet</title>
+        <meta name="description" content="STORY TESTNET FAUCET" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/newlogo.png" />
+        <link rel="icon" href="/story.jpeg" />
       </Head>
       <main>
         <Faucet />
