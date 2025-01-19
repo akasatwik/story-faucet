fc5a96527524fbd6781807e1a453e5672289bbd9
Satwik Samanta <85541670+akasatwik@users.noreply.github.com>
2025-01-19 20:31:05 +0530

    Update Faucet.tsx

--- components/Faucet.tsx
+++ components/Faucet.tsx
@@ -39,11 +39,11 @@ export default function Faucet() {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-            <img className="mx-auto h-12 w-auto" src="newlogo.png" alt="Monad devnet Faucet" />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Monad devnet Faucet</h2>
+            <img className="mx-auto h-12 w-auto" src="story.jpeg" alt="IP Faucet" />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">IP Faucet</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               <a href="https://x.com/problogger10" target="_blank" rel="noreferrer" className="font-medium text-indigo-600 hover:text-indigo-500">
-               Follow Me
+               Follow Me On Twitter
               </a>
             </p>
           </div>
@@ -58,7 +58,7 @@ export default function Faucet() {
             </div>
             <div>
               <button disabled={isDisabled} type="submit" className="disabled:opacity-25 group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                Request Funds
+                Claim 0.1 IP
               </button>
             </div>
           </form>
